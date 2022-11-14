91720aafc3e2743a71c01facef3100bfa074b54a
Hyperival <sanjaygautam779@gmail.com>
2022-11-15 00:05:41 +0530

    Snake And Ladder uc2

--- SnakeandLadder/SnakeLadder.cs
+++ SnakeandLadder/SnakeLadder.cs
@@ -10,9 +10,17 @@ namespace SnakeandLadder
     {
         static void Main(string[] args)
         {
-            int startPos = 0;
+            int Pos = 0;
             int numofPlayers = 0;
-            Console.WriteLine("single player at start position ");
+            Console.WriteLine("single player at start position :" + Pos);
+            RollDice();
+        }
+        public static void RollDice()
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            dice = dice + 1;
+            Console.WriteLine("Dice Num :" + dice);
         }
     }
 }
