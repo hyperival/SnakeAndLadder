e1c45f4bd26d8c26f67a39da8b646f484aabf1f9
Hyperival <sanjaygautam779@gmail.com>
2022-11-14 23:57:54 +0530

    Welcom message

---
+++ SnakeandLadder/SnakeLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            int startPos = 0;
+            int numofPlayers = 0;
+            Console.WriteLine("single player at start position ");
+        }
+    }
+}
