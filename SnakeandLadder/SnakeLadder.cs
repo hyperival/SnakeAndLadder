de4dcbd2118c1739b15f7d7ee06cbdfe6da2b82e
Hyperival <sanjaygautam779@gmail.com>
2022-11-16 19:57:17 +0530

    uc3 code added to Snakeandladderuc3

--- SnakeandLadder/SnakeLadder.cs
+++ SnakeandLadder/SnakeLadder.cs
@@ -4,23 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeandLadder
+namespace SNAKE_LADDER_BATCH222
 {
-    internal class SnakeLadder
+    internal class USERCASE
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int Pos = 0;
-            int numofPlayers = 0;
-            Console.WriteLine("single player at start position :" + Pos);
-            RollDice();
-        }
-        public static void RollDice()
-        {
+            int START_POSITION = 0;
+            int WIN_POSITION = 100;
+            int CURRENTPOSITION = 0;
+            const int forward = 1;
+            const int snakebite = 2;
+            Console.WriteLine("Welcome to snake and Ladder");
+            Console.WriteLine("Player will Start at Zero Position,Player position :{0}", START_POSITION);
             Random random = new Random();
-            int dice = random.Next(1, 7);
-            dice = dice + 1;
-            Console.WriteLine("Dice Num :" + dice);
+            Random random1 = new Random();
+            while (CURRENTPOSITION < WIN_POSITION)
+            {
+                int checkOptions = random1.Next(0, 3);
+                int dieRoll = random.Next(1, 7);
+                switch (checkOptions)
+                {
+                    case forward:
+                        {
+                            Console.WriteLine("Player will move forward");
+                            if (CURRENTPOSITION < 100)
+                            {
+                                CURRENTPOSITION += dieRoll;
+                            }
+
+                        }
+                        break;
+                    case snakebite:
+                        {
+                            Console.WriteLine("Player is bitten by a snake");
+                            if (CURRENTPOSITION > 0)
+                            {
+                                CURRENTPOSITION -= dieRoll;
+                            }
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("There will be no play. Player stays at same position");
+                        }
+                        break;
+
+                }
+                Console.WriteLine("CURRENTPOSITION is " + CURRENTPOSITION);
+
+
+            }
         }
     }
 }
